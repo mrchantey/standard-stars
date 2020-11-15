deda7719f4279051c9d8e9783c42b37156cca114
Peter Hayman <peteyhayman@gmail.com>
2020-11-15 19:44:19 +1100

    added camera feed and true north imu

---
+++ StandardStars/Assets/Packages/Ahoy.AR/Scripts/DeviceAttitude.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+namespace Ahoy.Android
+{
+
+	public class DeviceAttitude : MonoBehaviour
+	{
+		[Header("parent rotation must = (90,0,0)")]
+		public bool _;
+
+		void Start()
+		{
+			if (SystemInfo.supportsGyroscope)
+			{
+				Input.gyro.enabled = true;
+				Input.gyro.updateInterval = 0.0167f;// set the update interval to it's highest value (60 Hz)
+				Debug.Log($"DeviceAttitude - Gyro Initialized");
+			}
+			else
+				Debug.Log($"DeviceAttitude - Gyro Not Supported");
+		}
+		void Update()
+		{
+			var qrh = Input.gyro.attitude;// right handed to left handed
+			var qlh = new Quaternion(qrh.x, qrh.y, -qrh.z, -qrh.w);
+			transform.localRotation = qlh;
+		}
+	}
+}
---
+++ StandardStars/Assets/Packages/Ahoy.AR/Scripts/DevicePoseAggregator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// [ExecuteAlways]
+public class DevicePoseAggregator : MonoBehaviour
+{
+
+	public Transform poseAr;
+	public Transform poseImu;
+	public Transform originAgg;
+	public Transform poseAgg;
+
+	void Update()
+	{
+		var qAgg = poseImu.rotation * Quaternion.Inverse(poseAr.localRotation);
+		originAgg.rotation = qAgg;
+		var posAgg = originAgg.TransformPoint(poseAr.localPosition);
+		poseAgg.position = posAgg;
+		poseAgg.rotation = poseImu.rotation;
+	}
+}
